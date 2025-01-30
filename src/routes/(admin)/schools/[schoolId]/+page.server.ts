9c29f55ee03438485bc3c1dbb6cc1275b2697f65
rjhoskins <robertjhoskins@gmail.com>
2025-01-30 09:01:29 -0800

    add data func for district admin for school schoolIdpage

--- src/routes/(admin)/schools/[schoolId]/+page.server.ts
+++ src/routes/(admin)/schools/[schoolId]/+page.server.ts
@@ -27,6 +27,18 @@ export const load = async (event) => {
 			};
 		}
 	}
+	if (event.locals.user && event.locals.user.role === 'district_admin') {
+		const userId = event.locals.user.id;
+		if (userId) {
+			dataFunc = () => getSchoolForDistrictAdmin(Number(event.params.schoolId));
+			adminDataFunc = async () => {
+				if (event.locals.user) {
+					return { adminName: event.locals.user.name, adminEmail: event.locals.user.username };
+				}
+				return null;
+			};
+		}
+	}
 	if (event.locals.user && event.locals.user.role === 'super_admin') {
 		console.log('super_admin user =======================> ');
 		dataFunc = () => getSchoolForSuperAdmin(Number(event.params.schoolId));
@@ -74,6 +86,7 @@ const getSchoolForSuperAdmin = async (schoolId: number) => {
 		.select({
 			id: table.schoolsTable.id,
 			name: table.schoolsTable.name,
+			createdAt: table.schoolsTable.createdAt,
 			createdBy: table.schoolsTable.createdBy
 		})
 		.from(table.schoolsTable)
@@ -85,6 +98,30 @@ const getSchoolForSuperAdmin = async (schoolId: number) => {
 	// .where(is(table.schoolAdminsTable.userId, userId));
 
 	// const result = await db.select().from(users).innerJoin(pets, eq(users.id, pets.ownerId))
+};
+const getSchoolForDistrictAdmin = async (schoolId: number) => {
+	const [res] = await db
+		.select({
+			id: table.schoolsTable.id,
+			name: table.schoolsTable.name,
+			createdAt: table.schoolsTable.createdAt,
+			createdBy: table.schoolsTable.createdBy
+		})
+		.from(table.schoolsTable)
+		.innerJoin(
+			table.districtAdminsTable,
+			eq(table.districtAdminsTable.districtId, table.schoolsTable.districtId)
+		)
+		.where(eq(table.schoolsTable.isActive, true));
+
+	console.log('getSchoolForSuperAdmin res => ', res);
+	return res;
+	// 	db
+	//         .select({  name: schools.name, id: schools.id,   })
+	//   .from(schools)
+	//            .innerJoin(districtAdmins, eq(districtAdmins.districtId, schools.districtId))
+	//       .where(eq(schools.isActive, true))
+	//   .limit(20))
 };
 const getSchoolAdmin = async (schoolId: number) => {
 	const [res] = await db
