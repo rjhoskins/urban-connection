6eed9ba2e4f4053fd22e82806c02a7fe1cc3c61e
rjhoskins <robertjhoskins@gmail.com>
2025-04-04 16:36:12 -0700

    tweak

--- src/routes/urban-connection-project-assessment/+layout.svelte
+++ src/routes/urban-connection-project-assessment/+layout.svelte
@@ -6,12 +6,14 @@
 </script>
 
 <section class="">
-	<div class="max-w-theme-max mx-auto flex w-full items-center bg-white px-5 py-4">
-		<img class=" w-11" src="/img/urban-connection-logo.png" alt="" />
-		<p class=" text-xl text-[#45464E]">Urban Connection Project</p>
-		<p class=" temt-sm ml-auto rounded-md bg-[#371E98] px-1.5 py-0.5 font-normal text-white">
-			{data.tokenParseRes.name}
-		</p>
+	<div class="bg-white">
+		<div class="max-w-theme-max mx-auto flex w-full items-center bg-white px-5 py-4">
+			<img class=" w-11" src="/img/urban-connection-logo.png" alt="" />
+			<p class=" text-xl text-[#45464E]">Urban Connection Project</p>
+			<p class=" temt-sm ml-auto rounded-md bg-[#371E98] px-1.5 py-0.5 font-normal text-white">
+				{data.tokenParseRes.name}
+			</p>
+		</div>
 	</div>
 	{@render children?.()}
 </section>
